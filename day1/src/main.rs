aebc521bedff5c2acebda2ca6544041b156d1be5
Stephan Smola <smola@farbspender.de>
2022-12-02 22:22:09 +0100

    Day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+
+fn main() {
+    let file_path = "input.txt";
+    let contents = fs::read_to_string(file_path)
+        .expect("File not found");
+
+    let str_values = contents.split("\r\n").collect::<Vec<&str>>();
+    let mut values: Vec<i32> = Vec::new();
+
+
+    let mut sum: i32 = 0;
+    for v in str_values {
+
+        if v != "" {
+            let num: i32 = v.parse().unwrap();
+            sum += num;
+        } else {
+            values.push(sum);
+            sum = 0;
+        }
+    }
+
+    values.sort();
+    values.reverse();
+
+    println!("Max: {}", values[0]);
+    println!("Top 3: {}", values[0] + values[1] + values[2]);
+}
