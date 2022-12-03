e7fd89ecf140b79957b89044fea9cf6dc2993730
Stephan Smola <smola@farbspender.de>
2022-12-03 19:28:10 +0100

    Day3 - 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,45 @@
 
 
+fn split_compartments(input:String) -> (String, String) {
+    let len = input.len();
+    let right = len / 2;
+
+    return ( String::from(&input[0..right]), String::from(&input[right..]))
+}
+
+fn make_compartment_mask(input:String) -> u64 {
+    let mut mask: u64 = 0;
+    let base = 2u64;
+
+    for c in input.chars() {
+        let n:u32 = map_char(c);
+        mask = mask | base.pow(n);
+    }
+
+    mask
+}
+
+fn map_char(c: char) -> u32 {
+    let x = c as u32;
+    if x >=97  { x - 97 } else { x - 65 + 26 }
+}
+
+
 fn main() {
-    let data = commons::read_input("sampleInput.txt");
+    let data: Vec<String> = commons::read_input("input.txt");
+
+
+    let mut sum = 0;
 
     for d in data {
-        println!("{}", d);
+        let (left, right) = split_compartments(d);
+        let left_mask = make_compartment_mask(left);
+        let right_mask = make_compartment_mask(right);
+
+        let sim = left_mask & right_mask;
+
+        sum += sim.trailing_zeros() + 1;
     }
+
+    println!("{sum}")
 }
