a0a746594e18eb26be40b16555ec10034378b27b
Stephan Smola <smola@farbspender.de>
2022-12-10 13:27:38 +0100

    Day 9 - Part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fmt;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 use regex::Regex;
 use crate::Direction::{Down, Error, Left, Right, Up};
 
@@ -36,32 +36,72 @@ impl Display for Motion {
 }
 
 
-fn follow(input: &Vec<Motion>) -> HashMap<String, bool> {
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Clone for Position {
+    fn clone(&self) -> Self {
+        Position { x: self.x, y: self.y }
+    }
+}
+
+impl Display for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}", self.x, self.y)
+    }
+}
+
+impl Debug for Position {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}", self.x, self.y)
+    }
+}
+
+
+fn follow_step(hx: i32, hy: i32, tx: i32, ty: i32) -> (i32, i32) {
+    return if (hx - tx).abs() > 1 || (hy - ty).abs() > 1 {
+        (tx + (hx - tx).signum(),
+         ty + (hy - ty).signum())
+    } else {
+        (tx, ty)
+    };
+}
+
+fn follow(input: &Vec<Motion>, rope_length: usize) -> HashMap<String, bool> {
     let mut positions: HashMap<String, bool> = HashMap::new();
+    let mut rope: Vec<Position> = vec![Position { x: 0, y: 0 }; rope_length];
 
-    let mut hx: i32 = 0;
-    let mut hy: i32 = 0;
-    let mut tx: i32 = 0;
-    let mut ty: i32 = 0;
-
-    positions.insert(format!("{tx}|{ty}"), true);
+    let tail = rope.last().unwrap();
+    positions.insert(format!("{}|{}", tail.x, tail.x), true);
 
     for i in input {
         for _ in 0..i.steps {
+            let head = rope.get(0).unwrap();
+            let mut hx = head.x;
+            let mut hy = head.y;
+
             match i.direction {
                 Right => hx += 1,
                 Left => hx -= 1,
                 Up => hy -= 1,
                 Down => hy += 1,
-                Error => return positions
+                Error => {}
             }
 
-            if (hx - tx).abs() > 1 || (hy - ty).abs() > 1 {
-                tx += (hx - tx).signum();
-                ty += (hy - ty).signum();
+            rope[0] = Position { x: hx, y: hy };
+
+            for k in 1..rope_length {
+                let head = rope.get(k - 1).unwrap();
+                let tail = rope.get(k).unwrap();
+                let (tx, ty) = follow_step(head.x, head.y, tail.x, tail.y);
+
+                rope[k] = Position { x: tx, y: ty };
             }
 
-            positions.insert(format!("{tx}|{ty}"), true);
+            let tail = rope.last().unwrap();
+            positions.insert(format!("{}|{}", tail.x, tail.y), true);
         }
     }
 
@@ -101,7 +141,9 @@ fn main() {
 
     let input = parse(&data);
 
-    let res = follow(&input);
-
+    let res = follow(&input, 2);
     println!("Part One: {}", res.values().len());
+
+    let res2 = follow(&input, 10);
+    println!("Part Two: {}", res2.values().len());
 }
